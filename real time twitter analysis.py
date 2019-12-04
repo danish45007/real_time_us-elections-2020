aca87cf2eccf381ffd3d27f123ca0f969f243dfa
Danish Sharma <40580306+danish45007@users.noreply.github.com>
2019-12-04 18:33:50 +0530

    Add files via upload

---
+++ real time twitter analysis.py
@@ -0,0 +1,86 @@
+import tweepy
+from tweepy import Stream
+from tweepy import StreamListener
+import json
+from textblob import TextBlob
+import re
+import csv
+
+
+consumer_key = "F2j30sUAjjJ9DbJ2syuUPZ8fV"
+consumer_secret = "NwPt2uw0Nn5KdTzrF3tIyY4s7dyqSFg5u9w8gHFxJB8rT9bKkm"
+access_token = "1202176110458327041-3VCpfSk3MQ5JejuAtfayULbFO6pqZJ"
+access_token_secret = "NeZhjZrEE7dUmyw6718AfP2ZoSa4cs2CWPpuK4iAAUkl6"
+
+
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+### Get my timeline!!!1
+
+'''twittes = api.home_timeline()
+
+for tweet in twittes:
+    print(tweet.text)'''
+### Get twitts steam and do sentiment analysis
+
+
+trump = 0
+warren = 0
+
+header_name = ['Trump', 'Warren']
+with open('sentiment.csv', 'w') as file:
+    writer = csv.DictWriter(file, fieldnames=header_name)
+    writer.writeheader()
+
+
+class Listener(StreamListener):
+
+    def on_data(self, data):
+        raw_twitts = json.loads(data)
+        try:
+            tweets = raw_twitts['text']
+
+            tweets = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweets).split())
+            tweets = ' '.join(re.sub('RT', ' ', tweets).split())
+
+            blob = TextBlob(tweets.strip())
+
+            global trump
+            global warren
+
+            trump_sentiment = 0
+            warren_sentiment = 0
+
+            for sent in blob.sentences:
+                if "Trump" in sent and "Warren" not in sent:
+                    trump_sentiment = trump_sentiment + sent.sentiment.polarity
+                else:
+                    warren_sentiment = warren_sentiment + sent.sentiment.polarity
+
+            trump = trump + trump_sentiment
+            warren = warren + warren_sentiment
+
+            with open('sentiment.csv', 'a') as file:
+                writer = csv.DictWriter(file, fieldnames=header_name)
+                info = {
+                    'Trump': trump,
+                    'Warren': warren
+                }
+                writer.writerow(info)
+
+            print(tweets)
+            print()
+        except:
+            print('Error got')
+
+    def on_error(self, status):
+        print(status)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twitter_stream = Stream(auth, Listener())
+twitter_stream.filter(track = ['Trump', 'Warren'])
